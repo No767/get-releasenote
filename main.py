7cd0e12bd2d293b4f0a3797c56a7160296d15c40
Noelle Wang <73260931+No767@users.noreply.github.com>
2025-04-15 23:42:50 -0700

    Apply fixes
    * now it is working
    
    * fixes
    
    * version removed

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import Optional
 
 import msgspec
+from dotenv import load_dotenv
 from packaging.version import parse as parse_version
 
 VERSION_RE = re.compile(
@@ -127,6 +128,7 @@ class Parser:
             ),
             re.MULTILINE,
         )
+
         match = head_re.match(msg)
         if match is None:
             raise ValueError(
@@ -188,6 +190,7 @@ class Controller:
 
 
 def main() -> int:
+    load_dotenv()  # Shouldn't have that much of an impact
     controller = Controller(root=Path.cwd())
     controller.start()
     return 0
