62cab7a55691c9b5c0a03924e1cde5317f852931
No767 <73260931+No767@users.noreply.github.com>
2025-12-24 01:10:57 -0800

    Rewrite to account for blank environment strings, fix ambiguous error
    It should *finally* work
    
    Point it to latest commit #minor
    
    fix
    
    fixix
    
    making them explictly
    
    dont bother with checks
    
    no tag checks
    
    .
    
    .
    
    .
    
    testte
    
    t
    
    .
    
    .
    
    .
    
    .
    
    fix
    
    Remove changelog and output files

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ from pathlib import Path
 from typing import Optional
 
 import msgspec
-from dotenv import load_dotenv
+from dotenv import dotenv_values, find_dotenv
 from packaging.version import parse as parse_version
+
+ENV_VAR_LIST = [
+    "INPUT_CHANGES_FILE",
+    "INPUT_OUTPUT_FILE",
+    "INPUT_CHECK_REF",
+    "INPUT_NAME",
+    "INPUT_VERSION",
+    "INPUT_VERSION_FILE",
+    "INPUT_START_LINE",
+    "INPUT_HEAD_LINE",
+    "INPUT_FIX_ISSUE_REGEX",
+    "INPUT_FIX_ISSUE_REPL",
+]
 
 VERSION_RE = re.compile(
     "^{version} *= *{spec}".format(
@@ -19,27 +32,43 @@ VERSION_RE = re.compile(
 )
 
 
+class ActionInputs(msgspec.Struct, frozen=True):
+    name: str
+    version: Optional[str]
+    version_file: Optional[str]
+    changes_file: str
+    output_file: str
+    check_ref: Optional[str]
+    fix_issue_regex: Optional[str]
+    fix_issue_repl: Optional[str]
+    start_line: str = "<!-- towncrier release notes start -->"
+    head_line: str = r"##\s{name}\s\[{version}\]\(.*\)[\s-]*({date})"
+
+
 class Context(msgspec.Struct):
     root: Path
-    version: str = ""
+    version: Optional[str] = None
 
     def read_file(self, name: str) -> str:
+        self.root = Path(os.environ.get("GITHUB_WORKSPACE", "."))
         fname = self.root / name
         if not fname.exists():
-            raise ValueError(f"file '{name}' doesn't exist")
+            msg = f"file '{name}' doesn't exist (Path: {fname})"
+            raise ValueError(msg)
         return fname.read_text("utf-8")
 
 
 class Output(msgspec.Struct, frozen=True):
     version: str
-    pre_release: bool
-    dev_release: bool
 
     def show(self) -> None:
-        with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+        is_pre_release = parse_version(self.version).is_prerelease
+        is_dev_release = parse_version(self.version).is_devrelease
+
+        with Path.open(os.environ["GITHUB_OUTPUT"], "a") as fh:
             print(f"version={self.version}", file=fh)
-            print(f"prerelease={str(self.pre_release).lower()}", file=fh)
-            print(f"devrelease={str(self.dev_release).lower()}", file=fh)
+            print(f"prerelease={str(is_pre_release).lower()}", file=fh)
+            print(f"devrelease={str(is_dev_release).lower()}", file=fh)
 
 
 class Parser:
@@ -54,10 +83,12 @@ class Parser:
             return
         pre = "refs/tags/"
         if not head.startswith(pre):
-            raise ValueError(f"Git head '{head}' doesn't point at a tag")
+            msg = f"Git head '{head}' doesn't point at a tag"
+            raise ValueError(msg)
         tag = head[len(pre) :]
         if tag != version and tag != "v" + version:
-            raise ValueError(f"Git tag '{tag}' mismatches with version '{version}'")
+            msg = f"Git tag '{tag}' mismatches with version '{version}'"
+            raise ValueError(msg)
 
     def check_changes_version(self, declared_version: str, found_version: str) -> None:
         if declared_version == found_version:
@@ -66,30 +97,33 @@ class Parser:
         fver = parse_version(found_version)
 
         if dver < fver:
-            raise ValueError(
+            msg = (
                 f"The distribution version {dver} is older than "
                 f"{fver} (from '{self.changes_file}').\n"
                 "Hint: push git tag with the latest version."
             )
+            raise ValueError(msg)
 
-        else:
-            raise ValueError(
-                f"The distribution version {dver} is younger than "
-                f"{fver} (from '{self.changes_file}').\n"
-                "Hint: run 'towncrier' again."
-            )
+        msg = (
+            f"The distribution version {dver} is younger than "
+            f"{fver} (from '{self.changes_file}').\n"
+            "Hint: run 'towncrier' again."
+        )
+        raise ValueError(msg)
 
-    def find_version(self, ctx: Context, *, version_file: str, version: str) -> str:
-        if not version and not version_file:
-            raise ValueError("No one of 'version', 'version_file' is set")
-        if version:
-            if version_file:
-                raise ValueError("version and version_file arguments are ambiguous")
+    def find_version(
+        self, ctx: Context, *, version_file: Optional[str], version: Optional[str]
+    ) -> str:
+        if version is not None:
             return version
-        txt = ctx.read_file(version_file)
-        if match := VERSION_RE.search(txt):
-            return match.group(2)
-        raise ValueError(f"Unable to determine version in file '{version_file}'")
+
+        if version_file is not None:
+            txt = ctx.read_file(version_file)
+            if match := VERSION_RE.search(txt):
+                return match.group(2)
+
+        msg = f"Unable to determine version in file '{version_file}'"
+        raise ValueError(msg)
 
     def parse(
         self,
@@ -97,45 +131,46 @@ class Parser:
         *,
         start_line: str,
         head_line: str,
-        fix_issue_regex: str,
-        fix_issue_repl: str,
-        input_check_ref: str,
+        fix_issue_regex: Optional[str],
+        fix_issue_repl: Optional[str],
     ) -> str:
-        if (
-            fix_issue_regex
-            and not fix_issue_repl
-            or not fix_issue_regex
-            and fix_issue_repl
+        if (fix_issue_regex and not fix_issue_repl) or (
+            not fix_issue_regex and fix_issue_repl
         ):
             raise ValueError(
                 "fix_issue_regex and fix_issue_repl should be used together"
             )
-        self.check_head(ctx.version, input_check_ref)
+
+        if not ctx.version:
+            raise ValueError("Version failed to set when finding version")
+
         changes = ctx.read_file(self.changes_file)
 
         _, sep, msg = changes.partition(start_line)
         if not sep:
-            raise ValueError(
+            msg = (
                 f"Cannot find TOWNCRIER start mark ({start_line!r}) "
                 "in file '{changes_file}'"
             )
+            raise ValueError(msg)
 
         msg = msg.strip()
         head_re = re.compile(
             head_line.format(
-                version=r"\[(?P<version>[0-9][0-9.abcr]+(\.post[0-9]+)?)\]",
+                version=r"(?P<version>[0-9][0-9.abcr]+(\.post[0-9]+)?)",
                 date=r"\d+-\d+-\d+",
-                name=self.name,
+                name=re.escape(self.name) if self.name else ".*",
             ),
             re.MULTILINE,
         )
 
         match = head_re.match(msg)
         if match is None:
-            raise ValueError(
+            msg = (
                 f"Cannot find TOWNCRIER version head mark ({head_re.pattern!r}) "
                 f"in file '{self.changes_file}'"
             )
+            raise ValueError(msg)
         found_version = match.group("version")
         self.check_changes_version(ctx.version, found_version)
 
@@ -148,52 +183,55 @@ class Parser:
             msg = msg[match.end() :]
 
         if fix_issue_regex:
-            msg = re.sub(fix_issue_regex, fix_issue_repl, msg)
+            msg = re.sub(fix_issue_regex, fix_issue_repl or "", msg)
         return msg.strip()
 
 
-class Controller:
-    """Main controller for operations"""
+def sanitize_input(env_input: dict[str, str]) -> dict[str, Optional[str]]:
+    def _clean(value: str) -> Optional[str]:
+        if len(value) == 0:
+            return None
 
-    def __init__(self, root: Path):
-        self.root = root
-        self.ctx = Context(root)
-        self.parser = Parser(
-            changes_file=os.environ["INPUT_CHANGES_FILE"], name=os.environ["INPUT_NAME"]
-        )
+        return value
 
-    def start(self):
-        self.ctx.version = self.parser.find_version(
-            self.ctx,
-            version_file=os.environ["INPUT_VERSION_FILE"],
-            version=os.environ["INPUT_VERSION"],
-        )
-        version = parse_version(self.ctx.version)
-
-        note = self.parser.parse(
-            self.ctx,
-            start_line=os.environ["INPUT_START_LINE"],
-            head_line=os.environ["INPUT_HEAD_LINE"],
-            fix_issue_regex=os.environ["INPUT_FIX_ISSUE_REGEX"],
-            fix_issue_repl=os.environ["INPUT_FIX_ISSUE_REPL"],
-            input_check_ref=os.environ["INPUT_CHECK_REF"],
-        )
-
-        output = Output(
-            version=self.ctx.version,
-            pre_release=version.is_prerelease,
-            dev_release=version.is_devrelease,
-        )
-        output.show()
-
-        output_file = os.environ["INPUT_OUTPUT_FILE"]
-        (self.root / output_file).write_text(note)
+    return {k.removeprefix("INPUT_").lower(): _clean(v) for k, v in env_input.items()}
 
 
 def main() -> int:
-    load_dotenv()  # Shouldn't have that much of an impact
-    controller = Controller(root=Path.cwd())
-    controller.start()
+    # This is done as the .env follows GitHub actions's weird standard
+    # If a value is not specified, it becomes a blank string
+    env_inputs: dict[str, str] = (
+        {key: value for key, value in dict(os.environ).items() if key in ENV_VAR_LIST}
+        if not find_dotenv()
+        else dotenv_values(find_dotenv())
+    )  # type: ignore
+
+    root = Path(os.environ.get("GITHUB_WORKSPACE", "."))
+
+    sanitized_input = sanitize_input(env_inputs)
+    action_inputs = ActionInputs(**sanitized_input)
+    parser = Parser(changes_file=action_inputs.changes_file, name=action_inputs.name)
+
+    ctx = Context(root=root)
+    ctx.version = parser.find_version(
+        ctx, version_file=action_inputs.version_file, version=action_inputs.version
+    )
+
+    note = parser.parse(
+        ctx,
+        start_line=action_inputs.start_line,
+        head_line=action_inputs.head_line,
+        fix_issue_regex=action_inputs.fix_issue_regex,
+        fix_issue_repl=action_inputs.fix_issue_repl
+    )
+
+    output_file = root / action_inputs.output_file
+    output_file.write_text(note)
+
+    if os.environ.get("GITHUB_OUTPUT"):
+        output = Output(version=ctx.version)
+        output.show()
+
     return 0
 
 
